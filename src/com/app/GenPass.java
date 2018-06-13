6854b99da2ced5e140fd1423ba7c59a17ecc56a6
PrasadDaram77 <sprasad1166@gmail.com>
2018-06-13 14:58:55 +0530

    for knowing purpose

--- src/com/app/GenPass.java
+++ src/com/app/GenPass.java
@@ -3,7 +3,7 @@ package com.app;
 import java.util.UUID;
 
 public class GenPass {
-	
+	//this line add by for practice
 	public static String genPwd(){
 		
 		return UUID.randomUUID().toString().replaceAll("-","").substring(0, 8).toUpperCase();
